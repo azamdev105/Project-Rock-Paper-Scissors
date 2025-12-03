04b0ec97ba8ab3fa057016f3e80421ef55d466e4
azamdev105 <azammuhammad.1001@outlook.com>
2025-12-03 18:56:14 +0800

    Add if else logic

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 function computerChoice() {
-    return Math.floor((Math.random()*3)+1)    
+    let result = Math.floor((Math.random()*3)+1)
+    if(result === 1){
+        console.log('Computer: Rock')
+    } else if(result === 2) {
+        console.log('Computer: Paper')
+    } else {
+        console.log('Computer: Scissors')
+    }
+    return;    
 }
-
