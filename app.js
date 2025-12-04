2a8cbe9bf9d5fe9c511de6562ab549619920bad9
azamdev105 <azammuhammad.1001@outlook.com>
2025-12-04 18:58:37 +0800

    Resolve bugs and logical errors

--- app.js
+++ app.js
@@ -4,24 +4,49 @@ let computerScore = 0;
 
 //ComputerChoice
 function getComputerChoice() {
-    const randomNum = Math.floor((Math.random()*3)+1);
-    switch(randomNum) {
-        case 1:
-            console.log('Computer: Rock');
-            break;
-        case 2:
-            console.log('Computer: Paper');
-            break;
-        case 3:
-            console.log('Computer: Scissors');
-            break;   
+    let randomNum = Math.floor((Math.random()*3)+1);
+    let choice;
+    if(randomNum === 1) {
+        choice = 'rock'
+    } else if(randomNum === 2) {
+        choice = 'paper'
+    } else if(randomNum === 3) {
+        choice = 'scissors'
     }
-    return;
+    console.log(`Computer: ${choice}`);
+    
+    return choice;
 }
 
 //HumanChoice
 function getHumanChoice() {
     let userInput = prompt('“Rock”, “Paper” or “Scissors”');
     console.log(userInput);
+    return userInput.toLowerCase(); 
 }
-getHumanChoice()
+
+
+//playRound
+function playRound(humanChoice,computerChoice) {
+    // Make your function’s humanChoice parameter case-insensitive
+    if(humanChoice === computerChoice) {
+        console.log("It's a Draw!");
+    }
+    else if(humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You Won! Rock beats Scissors');
+    } else if(humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log('You Loss! Rock beats Scissors');
+    } else if(humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You Won! Scissors beats Paper');
+    } else if(humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log('You Loss! Scissors beats Paper');
+    } else if(humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You Won! Paper beats Rock');
+    } else if(humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log('You Loss! Paper beats Rock');
+    }
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+playRound(humanSelection,computerSelection)
